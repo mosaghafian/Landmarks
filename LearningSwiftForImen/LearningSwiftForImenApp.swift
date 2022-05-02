a32b1552ae0e69902a625ba354a48569b5854188
Mo Saghafian <msaghafian1@gmail.com>
2022-05-02 00:45:31 -0700

    Initial Commit

---
+++ LearningSwiftForImen/LearningSwiftForImenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftForImenApp.swift
+//  LearningSwiftForImen
+//
+//  Created by Mohammad Saghafian on 2022-05-02.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftForImenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
